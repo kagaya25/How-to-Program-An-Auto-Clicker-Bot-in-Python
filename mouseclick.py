31e3e3380059f3a3093d8a0531ea0abe42b309ff
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-08-17 00:18:15 +0900

    done
    done

--- mouseclick.py
+++ mouseclick.py
@@ -3,15 +3,15 @@ import multiprocessing
 import time
 print(pg.position())
 def foo():
-   pg.moveTo(520,511)
+   pg.moveTo(411,605)
    while True:
     pg.click()  
 if __name__ == '__main__':
     # Start foo as a process
     p = multiprocessing.Process(target=foo)
     p.start()
-    # Wait 10 seconds for foo
-    time.sleep(10)
+    # Wait 11 seconds for foo
+    time.sleep(11)
     # Terminate foo
     p.terminate()
     # Cleanup
